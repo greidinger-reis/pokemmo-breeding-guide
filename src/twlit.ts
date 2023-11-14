252aa34344382d16551ee0dcdde76d19699e3d06
greidinger-reis <jp.greidinger@hotmail.com>
2023-11-14 19:54:36 -0300

    Add minimist package to parse arguments

--- src/twlit.ts
+++ src/twlit.ts
@@ -1,51 +1,60 @@
-import { ArgsParser } from './args'
 import fs from 'fs'
 import { minify } from 'csso'
 import { config } from './config'
+import minimist from "minimist"
 
-const argv = Bun.argv
+function parseArgs(): [string, string, boolean] {
+	const argv = Bun.argv.slice(2)
+	const args = minimist(argv, {
+		boolean: 'watch',
+		default: {
+			watch: false,
+		},
+	})
 
-const args = new ArgsParser(argv.slice(2))
+	let input = args['input'] as string | undefined
+	let output = args['output'] as string | undefined
+	let watch = args['watch'] as boolean
 
-//@ts-ignore
-let input: string | undefined = args.input
-//@ts-ignore
-let output: string | undefined = args.output
-
-let watch: boolean = args.['watch'] ?? false
-
-//@ts-ignore
-if (!input || !output) {
-	console.log('Usage: twlit --input <file> --output <file>')
-	process.exit(1)
-}
-
-async function writeLitStyles(input: string, output: string) {
-	let contents = await Bun.file(input)
-		.text()
-		.catch(() => {
-			console.log(`Failed to read file ${input}. Might just not be created yet? retrying..`)
-			return ''
-		})
-
-	let cleanContents = contents.replaceAll('`', '')
-	cleanContents = cleanContents.replaceAll('\\', '\\\\')
-	if (config.env.NODE_ENV === 'production') {
-		cleanContents = minify(cleanContents).css
+	if (!input || !output) {
+		throw new Error('Usage: twlit --input <file> --output <file>')
 	}
 
-	const litContents = `import { css } from "lit";\nexport const TWStyles = css\` ${cleanContents} \``
-
-	await Bun.write(output, litContents)
-		.then(() => console.log(`🦊 TWLit - wrote to file ${output}`))
-		.catch(console.error)
+	return [input, output, watch]
 }
 
-await writeLitStyles(input!, output!)
+async function twLit() {
+	const [input, output, watch] = parseArgs()
 
-if(watch){
-	console.log(`🦊 Tw-lit: Watching ${input} and writing to ${output}`)
-	fs.watchFile(input, { interval: 1000 }, async () => {
-		await writeLitStyles(input!, output!)
-	})
+	async function writeLitStyles(input: string, output: string) {
+		let contents = await Bun.file(input)
+			.text()
+			.catch(() => {
+				console.log(`Failed to read file ${input}. Might just not be created yet? retrying..`)
+				return ''
+			})
+
+		let cleanContents = contents.replaceAll('`', '')
+		cleanContents = cleanContents.replaceAll('\\', '\\\\')
+		if (config.env.NODE_ENV === 'production') {
+			cleanContents = minify(cleanContents).css
+		}
+
+		const litContents = `import { css } from "lit";\nexport const TWStyles = css\` ${cleanContents} \``
+
+		await Bun.write(output, litContents)
+			.then(() => console.log(`🦊 Tw-Lit: Wrote to file ${output}`))
+			.catch(console.error)
+	}
+
+	await writeLitStyles(input, output)
+
+	if (watch) {
+		console.log(`🦊 Tw-lit: Watching ${input} and writing to ${output}`)
+		fs.watchFile(input, { interval: 1000 }, async () => {
+			await writeLitStyles(input, output)
+		})
+	}
 }
+
+await twLit()
